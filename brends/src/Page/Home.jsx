3f1b315678b10d9cd59783d523010063b6835046
nucikk <kirianuci@gmail.com>
2023-05-16 20:43:11 +0400

    Add timeline section guidelines and material information

--- brends/src/Page/Home.jsx
+++ brends/src/Page/Home.jsx
@@ -1,18 +1,42 @@
-import "../Style/home.css"
-import recentgle63 from "../Image/Rectangle 63.png"
-import recentgle64 from "../Image/Rectangle 64.png"
-import recentgle65 from "../Image/Rectangle 65.png"
+import "../Style/home.css";
+import bannerImage1 from "../Image/Rectangle 63.png";
+import bannerImage2 from "../Image/Rectangle 64.png";
+import bannerImage3 from "../Image/Rectangle 65.png";
+import timelineImage from "../Image/Rectangle 58.png";
 
 const Home = () => {
   return (
     <div className="home-wrapper">
-        <div className="home-banner">
-        <img src={recentgle63} alt="" />
-        <img src={recentgle64} alt="" />
-        <img src={recentgle65} alt="" />
-        </div>
-    </div>
-  )
-}
+      <div className="home-banner">
+        <img src={bannerImage1} alt="Banner 1" />
+        <img src={bannerImage2} alt="Banner 2" />
+        <img src={bannerImage3} alt="Banner 3" />
+      </div>
 
-export default Home
+      {/* Home Content */}
+      <div className="home-content">
+        <h1 className="home-heading">Brand Application</h1>
+        <button className="home-btn-topic">Brand Application Elements</button>
+
+        {/* Home Timeline */}
+        <div className="home-timeline">
+          <p>
+            Print materials: Guidelines for the use of the brand elements in
+            printed materials, such as business cards, brochures, and flyers.
+          </p>
+          <p>
+            Digital materials: Guidelines for the use of the brand elements in
+            digital materials, such as websites, social media, and emails.
+          </p>
+          <p>
+            Merchandise: Guidelines for the use of the brand elements in
+            merchandise, such as clothing and promotional items.
+          </p>
+          <img className="timeline-photo" src={timelineImage} alt="Timeline" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
