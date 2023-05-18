9f293c21200ac3fbc7b8e4510b5f3a75e7ade6fe
nucikk <kirianuci@gmail.com>
2023-05-18 20:54:05 +0400

    add informative section content

--- brends/src/Page/Informative.jsx
+++ brends/src/Page/Informative.jsx
@@ -1,11 +1,25 @@
-
-
+import "../Style/informative.css";
+import infobanner from "../Image/Rectangle 36.png"
 const Informative = () => {
   return (
-    <div>
+    <div className="info-wrapper">
+      <div className="info-content">
+        <div>
+            <h1 className="info-title">Brand Voice and Tone</h1>
+        <p className="info-description">
+          Our tone of voice is friendly, approachable, and professional. We aim
+          to communicate with our audience in a way that is both informative and
+          engaging. We avoid using overly technical language or jargon and
+          strive to communicate in a way that is easy to understand.
+        </p> 
+         <img src={infobanner} alt="info banner"/>
+        </div>
+      
+     
 
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Informative
+export default Informative;
