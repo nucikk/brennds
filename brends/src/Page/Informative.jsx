3948ad39bdca5a4452cfa334b223d2dfbb4143b7
nucikk <kirianuci@gmail.com>
2023-05-19 10:54:15 +0400

    Add right section photo and approachable section to info page

--- brends/src/Page/Informative.jsx
+++ brends/src/Page/Informative.jsx
@@ -1,22 +1,29 @@
 import "../Style/informative.css";
-import infobanner from "../Image/Rectangle 36.png"
+import infobanner from "../Image/Rectangle 36.png";
+import flowerBaneer from "../Image/Rectangle 38.png";
 const Informative = () => {
   return (
     <div className="info-wrapper">
       <div className="info-content">
         <div>
-            <h1 className="info-title">Brand Voice and Tone</h1>
-        <p className="info-description">
-          Our tone of voice is friendly, approachable, and professional. We aim
-          to communicate with our audience in a way that is both informative and
-          engaging. We avoid using overly technical language or jargon and
-          strive to communicate in a way that is easy to understand.
-        </p> 
-         <img src={infobanner} alt="info banner"/>
+          <h1 className="info-title">Brand Voice and Tone</h1>
+          <p className="info-description">
+            Our tone of voice is friendly, approachable, and professional. We
+            aim to communicate with our audience in a way that is both
+            informative and engaging. We avoid using overly technical language
+            or jargon and strive to communicate in a way that is easy to
+            understand.
+          </p>
+          <img src={infobanner} alt="Info Banner"  />
         </div>
-      
-     
-
+      </div>
+      <div className="informative-side-container">
+        <img src={flowerBaneer} alt="Flower Banner" />
+        <div className="informative-buttons">
+          <button>Friendly</button>
+          <button>Approachable</button>
+          <button>Professional</button>
+        </div>
       </div>
     </div>
   );
